aff6392edc8a29940ae3c962b2cb4361714714bc
wolf001122 <cnjszzy@gmail.com>
2026-01-02 10:39:26 +0800

    Delete functions/api/navigation.js

--- functions/api/navigation.js
+++
@@ -1,9 +0,0 @@
-export const onRequest = async ({ env }) => {
-  const navigation = await env.SITE_NAVIGATION.get("navigation");
-  if (!navigation) {
-    return new Response("Navigation not found", { status: 404 });
-  }
-  return new Response(navigation, {
-    headers: { "Content-Type": "application/json" }
-  });
-};
